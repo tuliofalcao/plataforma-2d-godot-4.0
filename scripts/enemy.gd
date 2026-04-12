98b47f757b2f9e1c8df05e3bd23497471b1799a4
tuliofalcao <falcao.tulio@gmail.com>
2026-04-12 00:05:28 -0300

    adding enemy

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,19 +1,26 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 700.0
 const JUMP_VELOCITY = -400.0
 
+@onready var wall_detector := $wall_detector as RayCast2D
+@onready var sprite := $texture as Sprite2D
 var direction := -1
-
-var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
-
-	velocity.x = direction * SPEED 
+	
+	# Rotaciona o raio para a direção que o inimigo está caminhando
+	wall_detector.target_position.x = 7 * direction
+	
+	if wall_detector.is_colliding():
+		direction *= -1
+	
+	# Vira a sprite na direção correta
+	sprite.flip_h = direction > 0
+	velocity.x = direction * SPEED * delta
 
 	move_and_slide()
