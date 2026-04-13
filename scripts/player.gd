97868bfd703f18f4aaad595c07cd7a1370f42053
tuliofalcao <falcao.tulio@gmail.com>
2026-04-13 02:30:42 -0300

    knockback

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,8 @@ const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
 var is_jumping := false
+var player_life := 10
+var knockback_vector := Vector2.ZERO
 @onready var animation := $anim as AnimatedSprite2D
 @onready var remote_transform := $remote as RemoteTransform2D
 
@@ -33,14 +35,37 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animation.play("idle")
+	
+	if knockback_vector != Vector2.ZERO:
+		velocity = knockback_vector
 
 	move_and_slide()
 
 
 func _on_hurtbox_body_entered(body: Node2D) -> void:
-	if body.is_in_group("enemies"):
+#	if body.is_in_group("enemies"):
+#		if velocity.y <= 0:
+#			queue_free()
+	if player_life < 0:
 		queue_free()
+	else:
+		if $ray_right.is_colliding():
+			take_damage(Vector2(-200,-200))
+		elif $ray_left.is_colliding():
+			take_damage(Vector2(200,-200))
 		
 func follow_camera(camera):
 	var camera_path = camera.get_path()
 	remote_transform.remote_path = camera_path
+	print("Caminho da camera atribuido: ", remote_transform.remote_path)
+
+func take_damage(knockback_force := Vector2.ZERO, duration := 0.25):
+	player_life -= 1
+	
+	if knockback_force != Vector2.ZERO:
+		knockback_vector = knockback_force
+		
+		var knockback_tween := get_tree().create_tween()
+		knockback_tween.parallel().tween_property(self, "knockback_vector", Vector2.ZERO, duration)
+		animation.modulate = Color(1,0,0,1)
+		knockback_tween.parallel().tween_property(animation, "modulate", Color(1,1,1,1), duration) 
