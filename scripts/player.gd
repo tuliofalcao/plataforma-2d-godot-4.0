80e8d6cc3900ab9b6853f14b1b20d04137436f7a
tuliofalcao <falcao.tulio@gmail.com>
2026-04-26 02:05:11 -0300

    set state function

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,8 +5,10 @@ const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
 var is_jumping := false
+var is_hurted := false
 var player_life := 10
 var knockback_vector := Vector2.ZERO
+var direction
 @onready var animation := $anim as AnimatedSprite2D
 @onready var remote_transform := $remote as RemoteTransform2D
 
@@ -24,22 +26,20 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+	direction = Input.get_axis("ui_left", "ui_right")
+	if direction !=0:
 		velocity.x = direction * SPEED
 		animation.scale.x = direction
-		if !is_jumping:
-			animation.play("run")
-	elif is_jumping:
-		animation.play("jump")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animation.play("idle")
 	
 	if knockback_vector != Vector2.ZERO:
 		velocity = knockback_vector
-
+	
+	_set_state()
 	move_and_slide()
+	
+	
 
 
 func _on_hurtbox_body_entered(body: Node2D) -> void:
@@ -68,4 +68,22 @@ func take_damage(knockback_force := Vector2.ZERO, duration := 0.25):
 		var knockback_tween := get_tree().create_tween()
 		knockback_tween.parallel().tween_property(self, "knockback_vector", Vector2.ZERO, duration)
 		animation.modulate = Color(1,0,0,1)
-		knockback_tween.parallel().tween_property(animation, "modulate", Color(1,1,1,1), duration) 
+		knockback_tween.parallel().tween_property(animation, "modulate", Color(1,1,1,1), duration)
+		
+	is_hurted = true 
+	await get_tree().create_timer(.3).timeout
+	is_hurted = false
+
+func _set_state():
+	var state = "idle"
+	
+	if !is_on_floor():
+		state = "jump"
+	elif direction != 0:
+		state = "run"
+		
+	if is_hurted:
+		state = "hurt"
+		
+	if animation.name != state:
+		animation.play(state)
